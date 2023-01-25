eabd028f67e58a0ca3a07b3e5b76bcc7e3101d17
Ethan Silvera <edsilverado2004@gmail.com>
2023-01-25 08:49:41 -0600

    Documentation Added

--- CuriousNumbers/CuriousNumbers.py
+++ CuriousNumbers/CuriousNumbers.py
@@ -6,19 +6,23 @@ num = 0
 i=1
 
 while True:
-    for letter in str(i):
-        digits.append(letter)
+    for thing in str(i):
+        #Seperate Digits
+        digits.append(thing)
     for x in range(0,len(digits)):
+        #Add Factorials of Individual Digits
         num = num + math.factorial(int(digits[x]))
     if(i==num):
-       fact.append(num)
-       num=0
-       digits.clear()
+        #Add Curious Number to List
+        fact.append(num)
+        num=0
+        digits.clear()
        
     else:
+        #Reset Variables
         digits.clear()
         num=0
     if(len(fact)==4):
         break
     i=i+1
-print(f"Curious Numbers: {fact}")
+print(f"Curious Numbers: {fact[0]}, {fact[1]}, {fact[2]}, {fact[3]}")
